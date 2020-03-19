a6546da4aeaf50ab8a542626ade0a5c35ec6f30c
HAYAMA_Kaoru <iyahaya@nifty.com>
2020-03-19 11:45:16 +0900

    installer/wix3: fix paths for renaming

--- wix3/make.cmd
+++ wix3/make.cmd
@@ -1,12 +1,6 @@
 @echo off
 setlocal
 set "PROMPT=$ "
-candle.exe | findstr "version.2"
-if errorlevel 1 (
-    set "WIX=wix3"
-) else (
-    set "WIX=wix2"
-)
 call :"%1"
 endlocal
 exit /b
@@ -17,19 +11,18 @@ exit /b
     exit /b
 
 :"amd64"
-    upx ..\cmd\amd64\nyagos.exe
-    call :mkmsi nyagos-amd64-%WIX%
+    call :mkmsi nyagos-amd64
     exit /b
 
 :"386"
-    upx ..\cmd\386\nyagos.exe
-    call :mkmsi nyagos-386-%WIX%
+    call :mkmsi nyagos-386
     exit /b
 
 :mkmsi
     candle "%~1.wxs" || exit /b 1
     light  "%~1.wixobj" || exit /b 1
     del "%~1.wixobj"
+    del "%~1.wixpdb"
     exit /b 0
 
 :"status"
